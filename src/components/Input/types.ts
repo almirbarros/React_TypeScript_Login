36fa859624ba16b12fc7b6205f62b93da7fe951a
Almir Barros <almirbarros@gmail.com>
2026-05-21 13:18:57 -0300

    ajuste

--- src/components/Input/types.ts
+++ src/components/Input/types.ts
@@ -1,6 +1,6 @@
 import type { ComponentPropsWithoutRef } from "react";
 import type { Control } from "react-hook-form";
-import type { IFormLogin } from "../../pages/Login/types"; // Mantenha o nome original do seu arquivo externo
+import type { IFormLogin } from "../../pages/Login/types";
 
 export interface InputProps extends ComponentPropsWithoutRef<"input"> {
   control: Control<IFormLogin, any>;
